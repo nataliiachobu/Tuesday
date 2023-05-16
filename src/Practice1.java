22870b6191989e8579631ad3fe344cc5c4288d92
nataliiachobu <mebel2008@yahoo.com>
2023-05-16 15:55:59 -0700

    added new changes to Practice1 class

--- src/Practice1.java
+++ src/Practice1.java
@@ -1,2 +1,19 @@
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 public class Practice1 {
+    public static void main(String[] args) {
+        Queue<Integer> list=new PriorityQueue<>();
+        list.add(2);
+        list.add(12);
+        list.add(22);
+        list.add(32);
+        list.add(42);
+        System.out.println(list);
+        System.out.println(list.contains(122));
+        System.out.println(list.poll());
+        System.out.println(list);
+
+
+    }
 }
